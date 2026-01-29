39598f2f9ff528380ba7bc06166fce90826b7eef
viduni marasingha <viduni2023@gmail.com>
2026-01-29 15:19:59 +0530

    change main.tf and update pem to terraform folder

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,17 +14,17 @@ resource "null_resource" "deploy_blood_donation_app" {
       "docker stop frontend_c || true",
       "docker rm frontend_c || true",
 
-      # Run backend
+      # Run backend container
       "docker run -d --restart unless-stopped -p 4000:4000 --name backend_c viduniamashani/project-backend:latest",
 
-      # Run frontend
+      # Run frontend container
       "docker run -d --restart unless-stopped -p 3000:3000 --name frontend_c --link backend_c:backend viduniamashani/project-frontend:latest"
     ]
 
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("/var/lib/jenkins/.ssh/vserver.pem")
+      private_key = file("${path.module}/vserver.pem")  # <-- PEM must be in the same folder as main.tf
       host        = "15.206.66.58"
     }
   }
