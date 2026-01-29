086fa2c599e5fbbac2e711d96c371dc2fc6be17a
viduni marasingha <viduni2023@gmail.com>
2026-01-29 13:52:03 +0530

    change variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,15 +7,18 @@ variable "aws_region" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.small"
 }
 
 variable "ami_id" {
   description = "Ubuntu AMI ID"
   type        = string
+  default     = "ami-0abcdef1234567890"  # replace with the correct Ubuntu AMI for Mumbai
 }
 
 variable "key_name" {
   description = "EC2 Key pair name"
   type        = string
+  default     = "vserver"
 }
+
